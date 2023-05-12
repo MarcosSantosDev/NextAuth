73a984ac732bd233f220ef3918f3d9642c7cedf3
MarcosSantosDev <marcosadriano034@gmail.com>
2023-05-11 22:30:46 -0300

    refactor(middleware): friendly redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,23 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { COOKIE_AUTH_TOKEN } from './common/constants/storage';
 
+const routes = {
+  guest: ['/signIn'],
+  private: ['/home']
+}
+
 export function middleware(request: NextRequest) {
-  const isSignInPage = request.nextUrl.pathname === '/signIn'
-  const isRootPathPage = request.nextUrl.pathname === '/'
+  const nextCookies = request.cookies;
+  const nextPathname = request.nextUrl.pathname;
+  const isAuthenticated = Boolean(nextCookies.get(COOKIE_AUTH_TOKEN));
+  const isRootPath = nextPathname === '/'
 
-  const authToken = request.cookies.get(COOKIE_AUTH_TOKEN);
-
-  const isAuthenticated = Boolean(authToken);
-
-  if (isAuthenticated && (isRootPathPage || isSignInPage)) {
-    return NextResponse.redirect(new URL('/home', request.url));
-  } else if (!isAuthenticated) {
-    if (!isSignInPage) {
-      return NextResponse.redirect(new URL('/signIn', request.url));
+  if (isAuthenticated) {
+    if (routes.guest.includes(nextPathname) || isRootPath) {
+      return NextResponse.redirect(new URL('/home', request.url));
     }
+  } else if (routes.private.includes(nextPathname)) {
+    return NextResponse.redirect(new URL('/signIn', request.url));
   }
 
   return NextResponse.next();
